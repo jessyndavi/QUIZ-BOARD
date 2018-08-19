88e4b550fba2dc8b8e5f09fff0ba41589b6578ee
jessy ndavi <jesssyndavi@gamil.com>
2018-08-19 22:47:49 +0300

    Apply changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,24 @@
 function check (){
 
-document.getElementbyId("after_submit").style.visibility ="visible";s
+var question1 = document.quiz.question1.value;
+var question2 = document.quiz.question2.value;
+var question3 = document.quiz.question3.value;
+var correct = 0;
 
+    if (question1 == "ECMAscript") {
+       correct = correct + 10;
+}
+    if (question2 == "ECMAscript") {
+       correct+15;
+}
+    if (question3 == "Hypertext Markup Language") {
+       correct+15;
+    }
+
+var messages = {"Great job1", "That is just okay", "You really need to do better"};
+var pictures = {"img/pNR5FDm.gif", "img/images.jpeg", "img/download.jpeg"};
+
+document.getElementbyId("after_submit").style.visibility ="visible";
+document.getElementbyId("correct_number").innerHTML = "You got " + correct + " correct .";
+document.getElementbyId("picture").src = pictures{score};
 }
