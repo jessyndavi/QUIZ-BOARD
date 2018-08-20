28e86455c2f4c0f56ff4fbae4b588871f614492b
jessyndavi <41961288+jessyndavi@users.noreply.github.com>
2018-08-20 17:16:09 +0300

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,48 @@
-function check (){
 
-var question1 = document.quiz.question1.value;
-var question2 = document.quiz.question2.value;
-var question3 = document.quiz.question3.value;
-var correct = 0;
+function check() {
+  var question1 = document.quiz.question1.value;
+  var question2 = document.quiz.question2.value;
+  var question3 = document.quiz.question3.value;
+  var question4 = document.quiz.question4.value;
+  var correct = 0;
 
-    if (question1 == "ECMAscript") {
-       correct = correct + 10;
-}
-    if (question2 == "ECMAscript") {
-       correct+15;
-}
-    if (question3 == "Hypertext Markup Language") {
-       correct+15;
-    }
+  if (question1 == "ECMAscript"){
+    correct+=20;
+  }
+  if (question2 == "A student"){
+    correct+=20;
+  }
+  if (question3 == "The gas pedal"){
+    correct+=20;
+  }
+  if (question4 == "Detailed"){
+    correct+=20;
+  }
 
-var messages = {"Great job1", "That is just okay", "You really need to do better"};
-var pictures = {"img/pNR5FDm.gif", "img/images.jpeg", "img/download.jpeg"};
+  var messages = ["EXCELLENT!","BEST!", "BETTER!", "GOOD!","MORE EFFORT NEEDED!"];
+  var pictures = ["img/excellent.gif", "img/best.gif", "img/better.gif", "img/good.gif", "img/moreEffort.gif"];
 
-document.getElementbyId("after_submit").style.visibility ="visible";
-document.getElementbyId("correct_number").innerHTML = "You got " + correct + " correct .";
-document.getElementbyId("picture").src = pictures{score};
+  var range;
+  if (correct < 20){
+    range = 4;
+  }
+  if (correct > 0 && correct < 40){
+    range = 3;
+  }
+  if (correct > 20 && correct < 60){
+    range = 2;
+  }
+  if (correct > 40 && correct < 80){
+    range = 1;
+  }
+  if (correct > 60){
+    range = 0;
+  }
+
+  document.getElementById("quiz").style.visibility = "hidden"
+  document.getElementById("after_submit").style.visibility = "visible";
+
+  document.getElementById("message").innerHTML = messages[range];
+  document.getElementById("number_correct").innerHTML = "Your score is " + correct + "!";
+  document.getElementById("picture").src = pictures[range]
 }
